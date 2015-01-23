2a0df5d7b626b301c2debdc48d152c64dee9f811
Thomas Sachse <thomas.sachse@ottogroup.com>
2015-01-23 13:33:17 +0100

    MotionTracker angefangen.

--- mpu6050-test.js
+++ mpu6050-test.js
@@ -1,5 +1,5 @@
-var mpu6050 = require('./lib/mpu6050');
-//var mpu6050 = require('./lib/mpu6050-fake');
+//var mpu6050 = require('./lib/mpu6050');
+var mpu6050 = require('./lib/mpu6050-fake');
 
 // Instantiate and initialize.
 var mpu = new mpu6050();
@@ -9,10 +9,10 @@ setInterval(function(){
   // Test the connection before using.
   if (mpu.testConnection()) {
     console.log(mpu.getMotion6());
-    console.log(mpu.getScaledRotation());
-    console.log(mpu.getScaledAcceleration()); 
-    console.log(mpu.getYRotation());
-    console.log(mpu.getXRotation());
+//    console.log(mpu.getScaledRotation());
+//    console.log(mpu.getScaledAcceleration()); 
+//    console.log(mpu.getYRotation());
+//    console.log(mpu.getXRotation());
   }
 } , 1000);
 
