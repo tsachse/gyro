ce1f74e98ba59d9912ee5471a1609e305ad69ce0
Thomas Sachse <thomas.sachse@web.de>
2015-01-25 18:02:01 +0100

    erstes Experiment mit THREE.js und angular -> nur bedingt erfolgtreich :-( Arbeitsstand gesichert.

--- public/monitor/monitor.js
+++ public/monitor/monitor.js
@@ -3,6 +3,24 @@ angular.module('monitor', [])
   .controller('MonitorCtrl', function ($scope, $http) {
 
     $scope.mpu = {};
+    
+    //$scope.scene = new THREE.Scene();
+
+    //$scope.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    //$scope.camera.position.z = 500;
+
+    //$scope.geometry = new THREE.BoxGeometry( 200, 100, 50 );
+    //$scope.material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+
+    //$scope.mesh = new THREE.Mesh( geometry, material );
+    //$scope.scene.add( mesh );
+    
+    //$scope.canvas = document.getElementById("accelCanvas");
+        
+    //$scope.renderer = new THREE.WebGLRenderer({canvas:canvas});
+    //$scope.renderer.setSize(canvas.width, canvas.height);
+    //$scope.renderer.render(scene, camera);
+    
     $scope.primus = new Primus();
     $scope.primus.on('data', function received(data) {
       $scope.mpu = data.mpu6050; 
