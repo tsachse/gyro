142aa0b6f4a70e84b1b0ef07d8d2e82312f7c620
sachse_t <thomas.sachse@ottogroup.com>
2015-01-26 13:05:13 +0100

    Die Grafik "wackelt" nun. :-)

--- public/monitor/monitor.js
+++ public/monitor/monitor.js
@@ -4,27 +4,33 @@ angular.module('monitor', [])
 
     $scope.mpu = {};
     
-    //$scope.scene = new THREE.Scene();
+    var scene = new THREE.Scene();
 
-    //$scope.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    //$scope.camera.position.z = 500;
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.z = 500;
 
-    //$scope.geometry = new THREE.BoxGeometry( 200, 100, 50 );
-    //$scope.material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+    var geometry = new THREE.BoxGeometry( 200, 100, 50 );
+    var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
 
-    //$scope.mesh = new THREE.Mesh( geometry, material );
-    //$scope.scene.add( mesh );
+    var mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
     
-    //$scope.canvas = document.getElementById("accelCanvas");
+    var canvas = document.getElementById("gyroCanvas");
         
-    //$scope.renderer = new THREE.WebGLRenderer({canvas:canvas});
-    //$scope.renderer.setSize(canvas.width, canvas.height);
-    //$scope.renderer.render(scene, camera);
+    var renderer = new THREE.WebGLRenderer({canvas:canvas});
+    renderer.setSize(canvas.width, canvas.height);
+    renderer.render(scene, camera);
+    $scope.renderer = renderer;
+    $scope.mesh = mesh;
     
     $scope.primus = new Primus();
     $scope.primus.on('data', function received(data) {
       $scope.mpu = data.mpu6050; 
       $scope.$apply();
+      $scope.mesh.rotation.x = $scope.mpu.gyro.delta[0];
+      $scope.mesh.rotation.y = $scope.mpu.gyro.delta[1];
+      $scope.mesh.rotation.z = $scope.mpu.gyro.delta[2];
+      $scope.renderer.render(scene, camera);
     });
 
   });
