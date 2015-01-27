defbc9a69291768892f33bbb88ce7db373a7de06
sachse_t <thomas.sachse@ottogroup.com>
2015-01-27 09:21:48 +0100

    Achsen und Grid gedreht ...

--- public/monitor/monitor.js
+++ public/monitor/monitor.js
@@ -9,15 +9,20 @@ angular.module('monitor', [])
     var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
     //camera.position.x = 500;
     camera.position.set( 300, 300, 300 );
+    // z-Achse nach oben
     camera.up = new THREE.Vector3( 0, 0, 1 );
     camera.lookAt(new THREE.Vector3( 0, 0, 0 ));
 
     var geometry = new THREE.BoxGeometry( 200, 100, 50 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+    var material = new THREE.MeshBasicMaterial( { color: 0xffff00, wireframe: true } );
 
     var mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
     scene.add(new THREE.AxisHelper(400));
+
+    var gridXY = new THREE.GridHelper(400, 30);
+    gridXY.rotation.x = Math.PI/2;
+    scene.add(gridXY)
     
     var canvas = document.getElementById("gyroCanvas");
         
@@ -41,3 +46,4 @@ angular.module('monitor', [])
 
 // http://jsfiddle.net/rohitghatol/tn9sm/
 // http://jsfiddle.net/NycWc/1/
+// http://danni-three.blogspot.de/2013/09/threejs-helpers.html
