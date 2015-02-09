6c5adf235d021c312ac326babd3d931c9bff7978
sachse_t <thomas.sachse@ottogroup.com>
2015-02-09 15:46:28 +0100

    USonicDistanceChecker eingebaut. Mal sehen, ob der Ansatz besser funktioniert.

--- usonic-test.js
+++ usonic-test.js
@@ -1,14 +1,16 @@
 //var usonic = require('./lib/usonic-fake');
-var usonic = require('r-pi-usonic');
-var DistanceChecker = require('./lib/distance-checker');
+//var usonic = require('r-pi-usonic');
+var USonicDistanceChecker = require('./lib/usonic-distance-checker');
 
-var sensor = usonic.createSensor(18, 17, 500);
-var checker = new DistanceChecker(sensor);
+//var sensor = usonic.createSensor(18, 17, 500);
+//var checker = new DistanceChecker(sensor);
+
+var checker = new USonicDistanceChecker(18,17,500);
 checker.run();
 
-setTimeout(function() {
-  console.log('Distance: ' + sensor().toFixed(2) + ' cm');
-}, 60);
+// setTimeout(function() {
+//  console.log('Distance: ' + sensor().toFixed(2) + ' cm');
+// }, 60);
 
 checker.on("data",function(data) {
   console.log(data);
