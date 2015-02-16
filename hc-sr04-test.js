10c315ba1818977f74b2420e35c6c0060cc4bc6f
sachse_t <thomas.sachse@ottogroup.com>
2015-02-16 11:44:11 +0100

    Ein weiterer Versuch mit r-pi-usonic.

--- hc-sr04-test.js
+++ hc-sr04-test.js
@@ -1,12 +1,17 @@
-//var usonic = require('./lib/usonic-fake');
-var usonic = require('r-pi-usonic');
+var usonic = require('./lib/usonic-fake');
+// var usonic = require('r-pi-usonic');
 var DistanceChecker = require('./lib/distance-checker');
 
 var sensor = usonic.createSensor(18, 17, 750);
 var checker = new DistanceChecker(sensor);
 
-
 setTimeout(function() {
  console.log('Distance: ' + sensor().toFixed(2) + ' cm');
+ checker.run();
 }, 60);
+
+checker.on("data",function(data) {
+  console.log(data);
+});
+
 
