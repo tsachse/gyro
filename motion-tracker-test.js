becccfcb64a64a28bd24878c925b3a27b25596e8
sachse_t <thomas.sachse@ottogroup.com>
2015-02-17 16:00:04 +0100

    init_data umgestellt

--- motion-tracker-test.js
+++ motion-tracker-test.js
@@ -13,11 +13,13 @@ var  primus = new Primus(server, {transformer: 'engine.io'});
 primus.library();
 primus.save(__dirname +'/public/js/primus.js');
 
-var mpu6050 = require('./lib/mpu6050');
-//var mpu6050 = require('./lib/mpu6050-fake');
+//var mpu6050 = require('./lib/mpu6050');
+var mpu6050 = require('./lib/mpu6050-fake');
 var MotionTracker = require('./lib/motion-tracker');
 
-
+var usonic = require('./lib/usonic-fake');
+//var usonic = require('r-pi-usonic');
+var DistanceChecker = require('./lib/distance-checker');
 
 var mpu = new mpu6050();
 var motion_tracker = new MotionTracker(mpu);
@@ -30,10 +32,6 @@ motion_tracker.on('data', function(data) {
   });
 });
 
-
-//var usonic = require('./lib/usonic-fake');
-var usonic = require('r-pi-usonic');
-var DistanceChecker = require('./lib/distance-checker');
 var sensor = usonic.createSensor(18, 17, 750);
 var checker = new DistanceChecker(sensor);
 checker.run();
