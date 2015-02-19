a1a6c8494c41c4ac4e45a620e8eda2223a2bf1e9
sachse_t <thomas.sachse@ottogroup.com>
2015-02-19 11:09:39 +0100

    Schreibfehler korrigiert.

--- mpu6050-log.js
+++ mpu6050-log.js
@@ -24,9 +24,9 @@ var i_id = setInterval(function(){
   // var data_arr = [141,142,143,144,145,146];
 
   var data = {
-    accl_x : data_arr[0],
-    accl_y : data_arr[1],
-    accl_z : data_arr[2],
+    accel_x : data_arr[0],
+    accel_y : data_arr[1],
+    accel_z : data_arr[2],
     gyro_x : data_arr[3],
     gyro_y : data_arr[4],
     gyro_z : data_arr[5],
