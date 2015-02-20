7960beb2880aff7ddcf607fd6b29226971d541cc
sachse_t <thomas.sachse@ottogroup.com>
2015-02-20 08:44:22 +0100

    mpu6050-fake kann nun Testdaten von aussen uebergeben bekommen.

--- motion-tracker-test.js
+++ motion-tracker-test.js
@@ -13,13 +13,13 @@ var  primus = new Primus(server, {transformer: 'engine.io'});
 primus.library();
 primus.save(__dirname +'/public/js/primus.js');
 
-var mpu6050 = require('./lib/mpu6050');
-//var mpu6050 = require('./lib/mpu6050-fake');
+//var mpu6050 = require('./lib/mpu6050');
+var mpu6050 = require('./lib/mpu6050-fake');
 var MotionTracker = require('./lib/motion-tracker');
 
-//var usonic = require('./lib/usonic-fake');
+var usonic = require('./lib/usonic-fake');
 //var usonic = require('r-pi-usonic');
-var usonic = require('./lib/hc-sr04');
+//var usonic = require('./lib/hc-sr04');
 var DistanceChecker = require('./lib/distance-checker');
 
 var mpu = new mpu6050();
